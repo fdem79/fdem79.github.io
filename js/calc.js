6136c1faf257d436a825ca75eebe24086452d0f9
fdem79 <flavien.demoron@iutsf.org>
2021-11-09 19:32:48 +0100

    Site initial

---
+++ js/calc.js
@@ -0,0 +1,35 @@
+let grammes = document.getElementById("grammes");
+let kilos = document.getElementById("kilos");
+let livres = document.getElementById("livres");
+
+grammes.addEventListener("input", function(){convPoids(this.id, this.value);});
+kilos.addEventListener("input", function(){convPoids(this.id, this.value);});
+livres.addEventListener("input", function(){convPoids(this.id, this.value);});
+
+function convPoids(id, valeur){
+    if(id == "grammes"){
+        kilos.value = valeur / 1000;
+        livres.value = valeur * 0.0022046;
+    }else if(id == "kilos"){
+        grammes.value = valeur * 1000;
+        livres.value = valeur * 2.2046;
+    }else if(id == "livres"){
+        grammes.value = valeur /0.0022046;
+        kilos.value = valeur / 2.2046;
+    }
+}
+
+
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect blal lelel lelle ls '; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("mach").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
